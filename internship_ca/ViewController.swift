310e3e2575dc03bab35c9dfc8c5305be565f34c2
sqalli <Mohammed@MacBook-Pro-of-Mohammed.local>
2017-05-28 10:36:33 +0000

    Initial Commit

---
+++ internship_ca/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  internship_ca
+//
+//  Created by Mohammed Tahri Sqalli on 28/05/2017.
+//  Copyright © 2017 Al Akhawayn University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
