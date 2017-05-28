77a7c31473b22aee353a6b6d48a50acb513ddfad
sqalli <Mohammed@MacBook-Pro-of-Mohammed.local>
2017-05-28 14:46:51 +0000

    Got all the info needed after the login was successful (Image URL, Email address...)

--- internship_ca/ViewController.swift
+++ internship_ca/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     let loginButton: FBSDKLoginButton = {
         let button = FBSDKLoginButton()
@@ -22,8 +22,48 @@ class ViewController: UIViewController {
         
         view.addSubview(loginButton)
         loginButton.center = view.center
+        loginButton.delegate = self
+        
+        // Application remembers that you are logged in
+        if let token = FBSDKAccessToken.current() {
+            fetchProfile()
+        }
+        
     }
-
+    
+    func fetchProfile(){
+        print ("fetch Profile")
+        
+        let parameters = ["fields" : "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (connection, result, error)-> Void in
+            if error != nil {
+                //onError()
+                print(error!)
+                return
+            }
+            
+            if let email = (result as AnyObject)["email"]! as? String {
+                print (email)
+            }
+            if let picture = (result as AnyObject)["picture"]! as? NSDictionary, let data = picture["data"] as? NSDictionary,let url = data["url"]! as? String
+            {
+                print(url)
+            }
+            
+        }
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print ("Completed Login")
+        fetchProfile()
+    }
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
